2ac11e855a72373f917d4ab8ec71963f3e1df5eb
Parid V <49834648+ParidV@users.noreply.github.com>
2023-07-18 20:30:14 +0200

    Added first test with docker

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import { ReactKeycloakProvider } from '@react-keycloak/web'
-import keycloak from './keycloak'
-import AppRouter from './routes/index'
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import AppRouter from "./routes/index";
+import keycloak from "./keycloak";
 
 const eventLogger = (event: unknown, error: unknown) => {
-  console.log('onKeycloakEvent', event, error)
-}
+  console.log("onKeycloakEvent", event, error);
+};
 
 const tokenLogger = (tokens: unknown) => {
-  console.log('onKeycloakTokens', tokens)
-}
+  console.log("onKeycloakTokens", tokens);
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,12 +19,12 @@ ReactDOM.render(
       onEvent={eventLogger}
       onTokens={tokenLogger}
     >
-      <div style={{margin: '0px', padding: '0px', width: '100%', height: '100%'}}>
+      <div
+        style={{ margin: "0px", padding: "0px", width: "100%", height: "100%" }}
+      >
         <AppRouter />
       </div>
     </ReactKeycloakProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
-
-
+  document.getElementById("root")
+);
