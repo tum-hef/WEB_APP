2d4bb488bba776920da5a33dbf3b87384f3076a8
syed-saad-zahidi <szahidi120@gmail.com>
2024-10-18 14:29:14 +0200

    url fixes

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -47,7 +47,7 @@ const Location = () => {
     try {
       const backend_url = process.env.REACT_APP_BACKEND_URL_ROOT; 
       const isDev = process.env.REACT_APP_NODE_ENV === 'development';   
-     const url = isDev ?  `${process.env.REACT_APP_BACKEND_URL_ROOT}:${frostServerPort}/FROST-Server/v1.0/Locations(${id})` : `https://${frostServerPort}-${backend_url}/FROST-Server/v1.0/Locations(${id})`
+     const url = isDev ?  `${process.env.REACT_APP_BACKEND_URL_ROOT}:${frostServerPort}/FROST-Server/v1.0/Locations(${id})` : `https://${frostServerPort}-${process.env.REACT_APP_FROST_URL}/FROST-Server/v1.0/Locations(${id})`
       axios
         .get(
           url,
