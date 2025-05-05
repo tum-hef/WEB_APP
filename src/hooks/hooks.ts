4e371f2f158912b1d3f55e25d2cdb4e574359b49
syed-saad-zahidi <szahidi120@gmail.com>
2025-05-05 18:47:28 +0200

    wrote a custom hook for permission handling

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -3,3 +3,11 @@ import type { RootState, AppDispatch } from '../store/store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useIsOwner = (): boolean => {
+    const selectedGroupId = useAppSelector((state) => state.roles.selectedGroupId);
+    const group = useAppSelector((state) =>
+      state.roles.groups.find((g) => g.group_name_id === selectedGroupId)
+    );
+    return group?.role === "owner";
+  };
